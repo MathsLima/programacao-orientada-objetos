b1fee984db9aa3ba4832e848f76543ace30cc6d9
Matheus Lima <75711232+MathsLima@users.noreply.github.com>
2024-06-30 19:53:30 -0300

    update geral na classe

--- avaliacoes/tf/addEntrega.java
+++ avaliacoes/tf/addEntrega.java
@@ -1,26 +1,32 @@
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class addEntrega extends JDialog {
+    private static final double valorFixo = 100.0;
     private Transportadora transportadora;
     private JTextField textFieldDistancia;
     private JComboBox<Caminhao> comboBoxCaminhao;
+    private JTextField textFieldQuantidaCargas;
 
     public addEntrega(Frame parent, Transportadora transportadora) {
         super(parent, "Adicionar Entrega", true);
         this.transportadora = transportadora;
         this.setLayout(new BorderLayout(10, 10));
-        this.setSize(300, 200);
+        this.setSize(400, 300);
 
-        JPanel panel = new JPanel(new GridLayout(3, 2, 10, 10));
+        JPanel panel = new JPanel(new GridLayout(4, 2, 10, 10));
 
         //botao distancia
         panel.add(new JLabel("Distância:"));
         textFieldDistancia = new JTextField();
         panel.add(textFieldDistancia);
+
+        //botao quantidade de cargas
+        panel.add(new JLabel("Quantidade de Cargas:"));
+        textFieldQuantidaCargas = new JTextField();
+        panel.add(textFieldQuantidaCargas);
 
         //botao caminhao
         panel.add(new JLabel("Caminhão:"));
@@ -38,8 +44,15 @@ public class addEntrega extends JDialog {
                 try {
                     double distancia = Double.parseDouble(textFieldDistancia.getText());
                     Caminhao caminhao = (Caminhao) comboBoxCaminhao.getSelectedItem();
+                    int quantidadeargas = Integer.parseInt(textFieldQuantidaCargas.getText());
 
                     Entrega entrega = new Entrega(distancia, caminhao);
+
+                    //add arga na entrega
+                    for(int i = 0; i < quantidadeargas; i++) {
+                        Carga carga = new Carga(valorFixo);
+                        entrega.adicionarCarga(carga);
+                    }
 
                     transportadora.adicionarEntrega(entrega);
                     JOptionPane.showMessageDialog(addEntrega.this, "Entrega adicionada com sucesso!");
