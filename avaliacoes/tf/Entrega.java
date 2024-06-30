6d709a3386f0da6dc4b351867b84429c913acc63
Matheus Lima <75711232+MathsLima@users.noreply.github.com>
2024-06-30 19:50:01 -0300

    adicionado valor fixo de carga

--- avaliacoes/tf/Entrega.java
+++ avaliacoes/tf/Entrega.java
@@ -1,13 +1,14 @@
 import java.util.*;
 
 public class Entrega {
-    private int cont = 0;
+    private static int cont = 0;
     private int id;
     private List<Carga> cargas;
     private double distancia;
     private Caminhao caminhao;
+    private static final double valorFixo = 100.0;
 
-    public Entrega(int id, List<Carga> cargas, double distancia, Caminhao caminhao) {
+    public Entrega(double distancia, Caminhao caminhao) {
         this.id = cont++;
         this.cargas = new ArrayList<>();
         this.distancia = distancia;
@@ -23,8 +24,26 @@ public class Entrega {
     }
 
     public double calcularValorTotal() {
-        return cargas.stream()
-                .mapToDouble(Carga::getValor)
-                .sum() * distancia;
+        return valorFixo * distancia * cargas.size();
+    }
+
+    public double getDistancia() {
+        return distancia;
+    }
+
+    public Caminhao getCaminhao() {
+        return caminhao;
+    }
+
+    public List<Carga> getCargas() {
+        return cargas;
+    }
+
+    public String toString() {
+        return "Entrega{" +
+                "id=" + id +
+                ", distancia=" + distancia +
+                ", caminhao=" + caminhao.toString() +
+                '}';
     }
 }
